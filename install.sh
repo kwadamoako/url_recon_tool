e1368fbeab0f1d7c544dbeef3fd084d657493d36
kwadamoako <137792889+kwadamoako@users.noreply.github.com>
2023-06-26 13:02:34 +0000

    Create install.sh
    "GO" will have to be installed before running this.
    If you come across any challenge installing 'gf',
    run:  go env -w GO111MODULE=off
    and rerun the installation file.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+go env -w GO111MODULE=off
+check_install() {
+        if ! command -v $1 &> /dev/null; then
+                echo "$1 is not installed. Installing..."
+                eval $2
+        else 
+             echo "$1 is already installed."
+        fi
+}
+
+check_install "assetfinder" "go get -u github.com/tomnomnom/assetfinder"
+check_install "waybackurls" "go install github.com/tomnomnom/waybackurls@latest"
+check_install "gau" "go install github.com/lc/gau/v2/cmd/gau@latest"
+check_install "httpx" "go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest"
+check_install "ffuf" "go install github.com/ffuf/ffuf/v2@latest"
+check_install "gf" "go get -u github.com/tomnomnom/gf"
+check_install "qsreplace" "go install github.com/tomnomnom/qsreplace@latest"
